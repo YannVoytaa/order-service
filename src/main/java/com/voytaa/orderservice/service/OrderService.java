398a4b84317427779bfd3e1a9546d875e73e12ff
Jan Wojtach <wojtachjanek@gmail.com>
2023-05-29 19:49:39 +0200

    Connect to inventory microservice

--- src/main/java/com/voytaa/orderservice/service/OrderService.java
+++ src/main/java/com/voytaa/orderservice/service/OrderService.java
@@ -1,11 +1,14 @@
 package com.voytaa.orderservice.service;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import com.voytaa.orderservice.dto.InventoryResponse;
 import com.voytaa.orderservice.dto.OrderLineItemsDto;
 import com.voytaa.orderservice.dto.OrderRequest;
 import com.voytaa.orderservice.model.Order;
@@ -20,6 +23,7 @@ import lombok.RequiredArgsConstructor;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -32,7 +36,26 @@ public class OrderService {
 
         order.setOrderLineItemsList(orderLineItems);
 
-        orderRepository.save(order);
+        List<String> skuCodes = order.getOrderLineItemsList()
+                .stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+
+        InventoryResponse[] inventoryResponses = webClient.get()
+                .uri("http://localhost:8082/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCodes", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        boolean allProductsInStock = Arrays.stream(inventoryResponses)
+                .allMatch(InventoryResponse::isInStock);
+
+        if (allProductsInStock) {
+            orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Product is not in stock, please try again later");
+        }
 
     }
 
